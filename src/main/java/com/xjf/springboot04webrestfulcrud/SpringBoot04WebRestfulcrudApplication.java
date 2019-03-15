2bb8e9c23c2093239bdebbff557415a0cee99a7e
fanrendale <1053314919@qq.com>
2019-03-15 15:44:14 +0800

    修改为war打包方式

--- src/main/java/com/xjf/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
+++ src/main/java/com/xjf/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
@@ -2,13 +2,19 @@ package com.xjf.springboot04webrestfulcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBoot04WebRestfulcrudApplication {
+public class SpringBoot04WebRestfulcrudApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBoot04WebRestfulcrudApplication.class);
+    }
 }
 
