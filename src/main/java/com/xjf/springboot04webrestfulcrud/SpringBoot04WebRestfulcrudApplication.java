b4c4eca0f8aa70977e55a639e296c2ba492cb2f2
fanrendale <1053314919@qq.com>
2019-03-20 11:05:30 +0800

    使用jar包方式

--- src/main/java/com/xjf/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
+++ src/main/java/com/xjf/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
@@ -5,16 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+ */
 @SpringBootApplication
-public class SpringBoot04WebRestfulcrudApplication extends SpringBootServletInitializer {
+public class SpringBoot04WebRestfulcrudApplication /*extends SpringBootServletInitializer*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
     }
 
-    @Override
+    /*@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SpringBoot04WebRestfulcrudApplication.class);
-    }
+    }*/
 }
 
